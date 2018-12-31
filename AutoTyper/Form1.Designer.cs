81832afc4583d4b952d5bb80b03715497dc5d2ab
ferit bölezek <feritbolezek@hotmail.com>
2018-12-31 05:55:20 +0100

    Init

---
+++ AutoTyper/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿using System.Windows.Forms;
+
+namespace AutoTyper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 57);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(409, 286);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(149, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Auto-typer";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(346, 393);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Execute";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 419);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "3000";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 403);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter start delay ms:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(129, 403);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(193, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Enter delay between lines ms (min 100):";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(137, 419);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(172, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.Text = "1000";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(15, 346);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 44);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Each line of text will be typed. In ANY textbox that has focus. Be careful!";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(278, 346);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Repeat for (amount of times):";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(294, 362);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(102, 20);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.Text = "5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(248, 25);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Repetition count: ?/?";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(346, 419);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 448);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private Label label6;
+        private Button button2;
+    }
+}
+
---
+++ AutoTyper/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace AutoTyper
+{
+    public partial class Form1 : Form
+    {
+        private System.Timers.Timer mainTimer = new System.Timers.Timer();
+
+        /** Amount of times the data in the textBox should be repeated. */
+        private int repetitionCount;
+
+        /** Amount of times the data in the textBox has been repeated. */
+        private int currentRepetition;
+
+        /** Amount of time before the data in the textBox is executed. */
+        private int startDelay;
+
+        /** Amount of time for the next line of data to be executed until the end of repeates. */
+        private int lineDelay;
+
+        private int currentLine;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ReadOptions();
+            button1.Enabled = false;
+            button2.Enabled = true;
+            label6.Text = "Repetition count: " + currentRepetition + "/" + repetitionCount;
+            mainTimer.Elapsed += new ElapsedEventHandler(OnTimedEvent);
+            mainTimer.Interval = startDelay;
+            mainTimer.Start();
+
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            mainTimer.Interval = lineDelay;
+
+            UpdateUI();
+
+            Write(); // {ENTER}
+
+            if (currentRepetition == repetitionCount)
+            {
+                Reset();
+            }
+        }
+
+        private void Reset() {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { Reset(); });
+                return;
+            }
+            mainTimer.Stop();
+            currentRepetition = 0;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            mainTimer.Elapsed -= new ElapsedEventHandler(OnTimedEvent);
+            label6.Text = "Repetition count: " + currentRepetition + "/" + repetitionCount;
+        }
+        private void UpdateUI() {
+
+            // Update UI on main thread.
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { UpdateUI(); });
+                return;
+            }
+            label6.Text = "Repetition count: " + currentRepetition + "/" + repetitionCount;
+        }
+
+        private void Write() {
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)delegate { Write(); });
+                return;
+            }
+            SendKeys.SendWait(richTextBox1.Lines[currentLine]);
+
+            currentLine++;
+
+            if (currentLine >= richTextBox1.Lines.Length)
+            {
+                currentRepetition++;
+                currentLine = 0;
+            }
+        }
+
+
+        /** Read preferences from textBoxes, repetition count, start delay etc. */
+        private void ReadOptions() {
+            int.TryParse(textBox3.Text,out repetitionCount);
+            int.TryParse(textBox2.Text, out lineDelay);
+            int.TryParse(textBox1.Text, out startDelay);
+
+            if (lineDelay < 100) lineDelay = 100;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Reset();
+        }
+    }
+}
